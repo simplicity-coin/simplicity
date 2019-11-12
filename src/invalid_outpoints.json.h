a5e60ed5855b29fee23e87b73bd0e91840390ad2
John <ComputerCraftr@users.noreply.github.com>
2019-11-11 18:04:08 -0700

    Upload missing files

---
+++ src/invalid_outpoints.json.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2018 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef SIMPLICITY_INVALID_OUTPOINTS_JSON_H
+#define SIMPLICITY_INVALID_OUTPOINTS_JSON_H
+#include <string>
+
+std::string LoadInvalidOutPoints()
+{
+    std::string str = "[\n"
+            "  {\n"
+            "    \"txid\": \"00405ad8cc4ec7b6be27dedc6bf19f2febf8e338031fe552d7bf5c0dfd6e67de\",\n"
+            "    \"n\": 0\n"
+            "  },\n"
+            "  {\n"
+            "    \"txid\": \"fff7164737e3437fd27b3787edeb2650eddd07966c492ffb7e0eb537c0a5b850\",\n"
+            "    \"n\": 1\n"
+            "  }\n"
+            "]";
+    return str;
+}
+
+#endif //SIMPLICITY_INVALID_OUTPOINTS_JSON_H
---
+++ src/invalid_serials.json.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2018 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+
+
+#ifndef SIMPLICITY_INVALID_SERIALS_JSON_H
+#define SIMPLICITY_INVALID_SERIALS_JSON_H
+
+#include <string>
+
+std::string LoadInvalidSerials()
+{
+    std::string str = "[\n"
+            "  {\n"
+            "    \"s\": \"c9c868bb56eacfc4f3d829528a0ae812dff26619cd38e6c9a0eea1eacddc84\"\n"
+            "  },\n"
+            "  {\n"
+            "    \"s\": \"198b62253217000fbab79bfe4bc4189c17c083ccab115866f16bf803946627107\"\n"
+            "  }\n"
+            "]";
+    return str;
+}
+
+#endif //SIMPLICITY_INVALID_SERIALS_JSON_H
