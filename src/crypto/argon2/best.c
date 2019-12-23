dca4355dfe45e8ea36d06bc9d2ad400fa4b12ed2
John <ComputerCraftr@users.noreply.github.com>
2019-12-23 00:24:50 -0700

    Remove files

--- src/crypto/argon2/best.c
+++
@@ -1,7 +0,0 @@
-#if defined (__x86_64__)
-#pragma message("Building Argon2 with SIMD optimisations")
-#include "opt.c"
-#else
-#pragma message("Building Argon2 without SIMD optimisations")
-#include "ref.c"
-#endif
